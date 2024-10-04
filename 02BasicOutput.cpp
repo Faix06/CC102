d60c25b25e9cb55f2cdf989241f28c68ecd062aa
Joy Magana <joymagana47@gmail.com>
2024-10-04 08:59:37 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Our lady of Lourdes College Foundation" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "BSIT - 1C" << endl;
+	cout << "Joy I Magana" << endl;
+	cout << "Purok-5 Napilihan, Vinzons, Camarines Norte" << endl;
+	return 0;
+}
