da300c8b9b9b4331b8f1c4b07810fc25cfe05aae
Joy Magana <joymagana47@gmail.com>
2024-11-06 10:27:09 +0800

    Add files via upload

---
+++ 00_IfStatement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char choice;
+	cout << "Is it raining? [y/n]: ";
+	cin >> choice;
+	if(choice == 'y') {
+		cout << "Get an umbrella.";
+	}
+	return 0;
+}
