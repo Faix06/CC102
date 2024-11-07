6364ab17e6ff718fd9351141827712fbc395e927
Joy Magana <joymagana47@gmail.com>
2024-11-07 12:41:01 +0800

    Add files via upload

---
+++ SAMPLE_CODE/00_ElseIfStatement.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char choice;
+	cout << "Is it raining? [y/n]: ";
+	cin >> choice;
+	if(choice == 'y') {
+		cout << "Get an umbrella and kapote.";
+	}
+	else if(choice == 'n') {
+		cout << "It is sunny.";
+	}
+	else {
+		cout << "Invalid Input.";
+	}
+	return 0;
+}
