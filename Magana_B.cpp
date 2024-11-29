6923b08d0362f904513a1e3b264a273799357b1a
Joy Magana <joymagana47@gmail.com>
2024-11-29 14:35:05 +0800

    Add files via upload

---
+++ Magana_B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int i = 0; i < 8; i++) {
+        for (int j = i; j < 8; ++j) {
+            cout << "#";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
