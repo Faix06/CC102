c23c8b8c5e37d8151af41f15ab8e066182426ff1
Joy Magana <joymagana47@gmail.com>
2024-11-30 14:34:05 +0800

    Update Magana_ACT-2.cpp

--- Magana_ACT-2.cpp
+++ Magana_ACT-2.cpp
@@ -4,23 +4,16 @@ using namespace std;
 int main() {
     char symbol1, symbol2, symbol3;
     int rows;
-
-    // Prompt the user for the first symbol
+    
     cout << "Enter 1st symbol to use (*, ^, #, $, @): ";
     cin >> symbol1;
-
-    // Prompt the user for the second symbol
     cout << "Enter 2nd symbol to use (*, ^, #, $, @): ";
     cin >> symbol2;
-
-    // Prompt the user for the third symbol
     cout << "Enter 3rd symbol to use (*, ^, #, $, @): ";
     cin >> symbol3;
-
     cout << "Enter the number of rows for the pattern: ";
     cin >> rows;
 
-    // Create a pattern using the three symbols
     cout << "Here is your pattern:\n";
     for (int i = 1; i <= rows; ++i) {
         for (int j = 1; j <= i; ++j) {
@@ -32,7 +25,7 @@ int main() {
                 cout << symbol3 << " ";
             }
         }
-        cout << endl; // Move to the next line
+        cout << endl;
     }
 
     return 0;
