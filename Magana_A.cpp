42dbfa4a0df7c1fb82df8bd5c0f273e7e37829f0
Joy Magana <joymagana47@gmail.com>
2024-11-30 15:13:05 +0800

    Add files via upload

--- Magana_A.cpp
+++ Magana_A.cpp
@@ -3,15 +3,10 @@ using namespace std;
 
 int main() {
     int rows = 8; // Number of rows
-    int cols = 8; // Number of columns
 
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if ((i + j) % 2 == 0) {
-                cout << " # *";
-            } else {
-                cout << "# *";
-            }
+        for (int j = 0; j < rows; j++) {
+            cout << "#";
         }
         cout << endl;
     }
